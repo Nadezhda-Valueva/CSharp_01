755677a8174aeaf6c24f066854f5fbe8fa8cf608
Nadezhda-Valueva <valuek627@gmail.com>
2024-05-13 09:18:45 +0300

    added lesson2:task2

---
+++ lesson2/task2/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = 10;
+int[] arr = new int[n];
+int i = 0; // cчетчик
+
+while (i < n) // (i <  arr.Lenght)
+{
+    arr[i] = i + 1;
+    // Console.Write(arr[i]);
+    // Console.Write(' ');
+    Console.Write($"{arr[i]} ");
+    i = i + 1;
+}

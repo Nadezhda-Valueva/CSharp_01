c22995afa6108860bf683c387e1a7e97ad3a1805
Nadezhda-Valueva <valuek627@gmail.com>
2024-05-16 19:48:05 +0300

    added lesson3:task2

---
+++ lesson3/task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Вoзвести в квадрат целое число от 1 до N.
+
+void PrintSquares (int limit)
+{
+    int i = 1;
+    while(i <= limit)
+    {
+        Console.Write($"{i*i} ");
+        i++;
+    }
+}
+
+PrintSquares(5);
+PrintSquares(10);
+PrintSquares(15);
